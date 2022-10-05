0a7af5d1941771bf0636b402e3b1073dc7fb97fe
Jair Ferraz <jairbferraz@outlook.com>
2022-10-05 17:10:39 -0300

    Evento click

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function iniciar() {
     MarioWorld.setAttribute("hidden","hidden");
     botaoIniciar.setAttribute("hidden", "hidden");
     document.addEventListener("keydown", jump);
+    document.addEventListener("click", jump);
     play.play();
 }
 
